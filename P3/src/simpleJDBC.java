2014738b416b76ddd4898d700e08ade10f75be88
constancefr <constance.frachon@gmail.com>
2024-03-15 11:51:21 -0400

    first p3 commit

---
+++ P3/src/simpleJDBC.java
@@ -0,0 +1,69 @@
+import java.sql.* ;
+import java.util.Scanner;
+
+class simpleJDBC
+{
+    public static void main ( String [ ] args ) throws SQLException
+    {
+        // Unique table names.  Either the user supplies a unique identifier as a command line argument, or the program makes one up.
+        String tableName = "";
+        int sqlCode=0;      // Variable to hold SQLCODE
+        String sqlState="00000";  // Variable to hold SQLSTATE
+
+        if ( args.length > 0 )
+            tableName += args [ 0 ] ;
+        else
+            tableName += "exampletbl";
+
+        // Register the driver.  You must register the driver before you can use it.
+        try { DriverManager.registerDriver ( new com.ibm.db2.jcc.DB2Driver() ) ; }
+        catch (Exception cnfe){ System.out.println("Class not found"); }
+
+        // This is the url you must use for DB2.
+        //Note: This url may not valid now ! Check for the correct year and semester and server name.
+        String url = "jdbc:db2://winter2024-comp421.cs.mcgill.ca:50000/comp421";
+
+        //REMEMBER to remove your user id and password before submitting your code!!
+        String your_userid = null;
+        String your_password = null;
+        //AS AN ALTERNATIVE, you can just set your password in the shell environment in the Unix (as shown below) and read it from there.
+        //$  export SOCSPASSWD=yoursocspasswd
+        if(your_userid == null && (your_userid = System.getenv("SOCSUSER")) == null)
+        {
+            System.err.println("Error!! do not have a password to connect to the database!");
+            System.exit(1);
+        }
+        if(your_password == null && (your_password = System.getenv("SOCSPASSWD")) == null)
+        {
+            System.err.println("Error!! do not have a password to connect to the database!");
+            System.exit(1);
+        }
+        Connection con = DriverManager.getConnection (url,your_userid,your_password) ;
+        Statement statement = con.createStatement ( ) ;
+
+
+        // TODO
+        printMenu();
+
+        // Parse input and read arguments
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
+
+        if (userInput.equals("1")) {
+            // call method 1
+        } else if (userInput.equals("2")) {
+            // etc.
+        } else {
+            // print error message
+            // reprint menu
+        }
+
+        // Finally but importantly close the statement and connection
+        statement.close ( ) ;
+        con.close ( ) ;
+    }
+
+    public static void printMenu() {
+        // TODO
+    }
+}
